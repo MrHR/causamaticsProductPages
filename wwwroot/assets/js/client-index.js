9469fe81e0c5718600c4aa1b98d2f2da14a085d4
Read Dennis <read.dennis92@gmail.com>
2020-10-30 17:09:41 +0100

    client index js update

--- wwwroot/assets/js/client-index.js
+++ wwwroot/assets/js/client-index.js
@@ -3,61 +3,43 @@ js regarding client interface (index.html)
 */
 
 
-wentFullscreenOnStartup = false;
-
 /* 
-fullscreen functions
+toggle full screen by clicking on animation image
 */
 document.getElementById("animation_image").onclick = function() {
-    toggleFullScreen();
+  toggleFullScreen();
 }
 
+/* 
+toggle full screen by clicking on image container (once at startup)
+*/
+wentFullscreenOnStartup = false;
 document.getElementById("image_container").onclick = function() {
-  if(!wentFullscreenOnStartup) {
-    toggleFullScreen();
-    wentFullscreenOnStartup = true;
-  }
+if(!wentFullscreenOnStartup) {
+  toggleFullScreen();
+  wentFullscreenOnStartup = true;
+}
 }
 
-function toggleFullScreen() {
-    var doc = window.document;
-    var docEl = doc.documentElement;
-
-    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-    var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-
-    if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-        requestFullScreen.call(docEl);
-        document.getElementById("toggle_button").classList.add("player_button_active");
-    }
-    else {
-        cancelFullScreen.call(doc);
-        document.getElementById("toggle_button").classList.remove("player_button_active");
-    }
-}
 
 /*
-var elem = document.documentElement;
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) { // Firefox 
-    elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) { // Chrome, Safari & Opera
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { // IE/Edge 
-    elem.msRequestFullscreen();
+toggle full screen function
+*/
+function toggleFullScreen() {
+  document.getElementById("image_container").focus();
+
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+      requestFullScreen.call(docEl);
+      document.getElementById("toggle_button").classList.add("player_button_active");
+  }
+  else {
+      cancelFullScreen.call(doc);
+      document.getElementById("toggle_button").classList.remove("player_button_active");
   }
 }
-
-function closeFullscreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.mozCancelFullScreen) {
-    document.mozCancelFullScreen();
-  } else if (document.webkitExitFullscreen) {
-    document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) {
-    document.msExitFullscreen();
-  }
-}*/
